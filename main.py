fa5d3bc9511e12f785814983be54033771698d53
Joakim Nilsson <94443280+Joakim-Nilsson@users.noreply.github.com>
2023-03-12 10:32:37 +0100

    Added a scheduler and refactored the code.

--- main.py
+++ main.py
@@ -1,39 +1,25 @@
-# Api Key: eca72bf95e2d4f4c910aa058209534db
-import requests
-from pprint import pprint
+import yagmail
+import pandas
+from news import NewsFeed
+import datetime
 
-class NewsFeed:
-    """Representing multiple news titles and links as a single string"""
-    api_key = "apiKey=eca72bf95e2d4f4c910aa058209534db"
-    base_url = "https://newsapi.org/v2/everything?"
+while True:
+    # Execute program at 17.43
+    if datetime.datetime.now().hour == 17 and datetime.datetime.now().minute == 43:
 
-    def __init__(self,interest, from_date, to_date, language):
-        self.interest = interest
-        self.from_date = from_date
-        self.to_date = to_date
-        self.language = language
+        df = pandas.read_excel('people.xlsx')
 
-    def get(self):
-        pass
+        for index, row in df.iterrows():
+            today = datetime.datetime.now().strftime('%Y-%m-%d')
+            yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+            news_feed = NewsFeed(interest=row['interest'],
+                                 from_date=yesterday,
+                                 to_date=today)
 
+            email = yagmail.SMTP(user="youraccount@gmail.com", password="YourPassword")
+            email.send(to=row['email'],
+                       subject=f"Your {row['interest']} news for today!",
+                       content=f"Hi {row['name']} \n See what´s on about {row['interest']} today. \n{news_feed.get()}\n Joakim")
 
-url = "https://newsapi.org/v2/everything?" \
-       "qInTitle=tesla&" \
-       "from=2023-02-10&" \
-       "to=2023-03-10&" \
-       "language=en&" \
-       "apiKey=eca72bf95e2d4f4c910aa058209534db"
-
-response = requests.get(url)
-content = response.json()
-articles = content['articles']
-
-email_body = ''
-for article in articles:
-    email_body = email_body + article['title'] + "\n" + article['url'] + "\n\n"
-
-print(email_body)
-
-
-
+    time.sleep(60)  #paus the execution for 60 seconds
 
